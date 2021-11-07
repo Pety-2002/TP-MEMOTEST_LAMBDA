7b8b580a3521ad75e18b3a4d0ad25203c48ea591
MiltonFernandez <miltonfernandez1254@hotmail.com>
2021-11-07 18:19:36 +0100

    Interfaz

--- memotest-lambda.py
+++ memotest-lambda.py
@@ -1,74 +1,119 @@
-    
-# en el caso de que las "cartas" con las que jugar esten predefinidas con 2 pares (etapa 1)
-"""lista_cartas=["A","B","A","B"]"""
-
-# en el caso de que las "cartas" con las que jugar esten predefinidas con 4 pares (etapa 4)
-"""lista_cartas=["A","A","B","B","C","C","D","D"]"""
-
-
+import random
+from tkinter import *
 
 lista_cartas = ["a","a","b","b","c","c","d","d","e","e","f","f","g","g","h","h"]
 LISTA_VACIA = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
 lista_juego = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
 
-#para mezclar las "cartas" y se encuentren en diferentes posiciones en c/partida (se mantienen los caracteres) (etapa 4)
-def mezclar_cartas (lista_original):
+
+def mezclar_cartas(lista_original):
+    '''
+    para mezclar las "cartas" y se encuentren en diferentes posiciones en c/partida (se mantienen los caracteres) (etapa 4)
+    Creada por: ...
+    '''
     lista_mezclada = lista_original
-    import random
     random.shuffle(lista_mezclada)
     return lista_mezclada
 
-#para filtrar los valores que deben ingresar en el juego (etapa 2) (me da error en el filatrado de numeros "numero.isnumeric")
 def validar_ingreso(numero):
-    if numero.isdigit():
-        if 0<int(numero)<len(lista_cartas)+1:
-            devolver=True
-        else:
-            devolver=False
-    else:
-        devolver=False
-    return devolver
+    '''
+    Filtrar los valores que deben ingresar en el juego (etapa 2).
+    Si el participante ingresa:
+    ● un valor que no corresponda a una posición
+    ● o el mismo no se encuentre disponible,
+    ● o no se trata de un valor numérico,
+    se le debe mostrar un mensaje acorde y solicitar el ingreso de un nuevo valor. 
+    Creada por: (algun otro), JuanP
+    '''
+    while not numero.isdigit() or int(numero)<=0 or int(numero)>len(lista_cartas):
+        numero = input("ERROR 401 :) /Escribir un NUMERO correspondiente a la posicion de la ficha deseada en el tablero: ")
+    return int(numero)
 
-#juego base
-def juego(lista_juego, lista_cartas):
+def imprimir_tablero(lista_juego):
+    '''
+    Imprimo el tablero
+    Creada por: JuanP
+    '''
+    #me ahorro todo esto -> print(lista_juego[:4], "\n", lista_juego[4:8], "\n", lista_juego[8:12], "\n", lista_juego[12:16], sep = "")
+    for contador in range(0,len(lista_juego),4):
+        print(lista_juego[contador:4+contador])
+    return
+
+def mezclar_jugadores():
+    lista = [cuadro_nombre1.get(), cuadro_nombre2.get()]
+    random.shuffle(lista)
+    return var.set(f"Es el turno de {lista[0]}")
+
+raiz = Tk()
+raiz.title("Login Lambda")
+raiz.resizable(0,0)
+raiz.config(bg="black")
+mi_frame=Frame(raiz,width=300, height=130)
+mi_frame.pack()
+mi_frame.config(bg="red")
+
+nombre_usuario1=Label(mi_frame, text="Nombre Usuario 1: ")
+nombre_usuario1.grid(row=0, column=0, padx = 10, pady =10)
+
+nombre_usuario2=Label(mi_frame, text="Nombre Usuario 2: ")
+nombre_usuario2.grid(row=1, column=0, padx = 10, pady =10)
+
+cuadro_nombre1=Entry(mi_frame)
+cuadro_nombre1.grid(row=0,column=1,padx = 10, pady =10)
+
+cuadro_nombre2=Entry(mi_frame)
+cuadro_nombre2.grid(row=1,column=1,padx = 10, pady =10)
+
+var = StringVar()
+
+Dicc_show=Label(mi_frame, textvariable = var)
+Dicc_show.grid(row=2, column=0, padx = 10, pady =10)
+Dicc_show.config(bg = "red", fg = "white")
+
+boton=Button(raiz,text="Enviar", command = mezclar_jugadores)
+boton.pack()
+
+raiz.mainloop()
+
+
     
+def juego(lista_juego, lista_cartas):
+    '''
+    juego base
+    Creada por: ...
+    '''
     lista_cartas= mezclar_cartas(lista_cartas)
     contador_intentos=0
         
     while lista_juego != lista_cartas:
         #comienzo del juego
-        print(lista_juego[:4], "\n", lista_juego[4:8], "\n", lista_juego[8:12], "\n", lista_juego[12:16], sep = "")
-        primera_posicion= input("Seleccione una posición: ")
-        while validar_ingreso(primera_posicion) == False:
-            print("Seleccione un numero del 1 al",len(lista_cartas))
-            primera_posicion= input("Seleccione una posición: ")
-        primera_posicion = int(primera_posicion)
+        imprimir_tablero(lista_juego)
+        primera_posicion= input("Seleccione una posición: ")  
+        primera_posicion = validar_ingreso(primera_posicion)
         lista_juego[primera_posicion-1] = lista_cartas[primera_posicion-1]
-        print(lista_juego[:4], "\n", lista_juego[4:8], "\n", lista_juego[8:12], "\n", lista_juego[12:16], sep = "")
-        #1er opcion
+        imprimir_tablero(lista_juego)
         
         segunda_posicion= input("Seleccione una segunda posición: ")
-        while validar_ingreso(segunda_posicion) == False:
-            print("Seleccione un numero del 1 al",len(lista_cartas))
-            segunda_posicion= input("Seleccione una posición: ")
-        segunda_posicion = int(segunda_posicion)
+        segunda_posicion = validar_ingreso(segunda_posicion)
         lista_juego[segunda_posicion-1] = lista_cartas[segunda_posicion-1]
-        print(lista_juego[:4], "\n", lista_juego[4:8], "\n", lista_juego[8:12], "\n", lista_juego[12:16], sep = "")
-        #2da opcion
+        imprimir_tablero(lista_juego)
         
+        #Si las fichas son distintas:
         if lista_juego[primera_posicion-1] != lista_juego[segunda_posicion-1]:
             lista_juego[primera_posicion-1] = LISTA_VACIA[primera_posicion-1]
             lista_juego[segunda_posicion-1] = LISTA_VACIA[segunda_posicion-1]
-        #condicion de que ambas opciones sean iguales
-        
+            
+        print("")
         contador_intentos+=1
+        
         #para contar la cantidad de intentos requeridos (etapa 3)
     return contador_intentos
 
 def main():
+    '''Creada por: ...'''
     seguir = "s"
     while seguir == "s":
-        print("usted gano en",juego(lista_juego, lista_cartas),"intentos")
+        print(f"Usted gano en {juego(lista_juego, lista_cartas)} intentos") #usar f print
         seguir= input("¿Seguir jugando?(s/n): ")
 
 main()
