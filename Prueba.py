62a9b47d6a3acc8e667d188aa12592956d530313
MiltonFernandez <miltonfernandez1254@hotmail.com>
2021-11-16 18:17:20 +0100

    cambios en la interfaz

--- Prueba.py
+++
@@ -1,7 +0,0 @@
-nombre1 = input(str("Ingrese nombre 1: "))
-nombre2 = input(str("Ingrese nombre 2: "))
-
-while nombre1 == '' and nombre2 == '':
-    nombre1 = input(str("Ingrese nombre 1: "))
-    nombre2 = input(str("Ingrese nombre 2: "))
-
